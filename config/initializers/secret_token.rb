414dd220eb3e77271870af8976e4eff6db29b6a0
GavMo <quavmo@gmail.com>
2012-07-30 11:00:27 -0700

    Init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PollStar::Application.config.secret_token = 'f0d86693090ae78a14a0913708d296d1cab762c4b78a36a69245f1f152dddfb56508d8c7461b5a487b83ed54c6070049545e3034ab7bb753479ed8a0751f7e2d'
